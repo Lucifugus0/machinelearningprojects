44e83ce02f86c164f77d8dfb7473fc1dcda36ea5
Lucifugus0 <130909171+Lucifugus0@users.noreply.github.com>
2025-03-04 16:57:11 +0700

    04-03-2025#2

--- src/components/pages/CapturePage.js
+++ src/components/pages/CapturePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import WebcamCapture from '../organisms/WebcamCapture/WebcamCapture';
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 
 const CapturePage = ({ setCapturedImage }) => {
   const navigate = useNavigate();
